5e446b31b8f4b9be852c823fdedc85b7224865c6
songzhikai <songzhikai@bonc.com.cn>
2019-03-21 10:21:40 +0800

    提交

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,4 +1,12 @@
 
-export default {
-  "disableCSSModules": true,
-};
+// export default {
+//   "disableCSSModules": true,
+// };
+
+//加载mock的数据 通过循环把在 mock 文件夹下的所有配置文件都拿到，并最后export出去
+
+const mock = {}
+require('fs').readdirSync(require('path').join(__dirname + '/mock')).forEach(function(file) {
+  Object.assign(mock, require('./mock/' + file))
+})
+module.exports = mock
